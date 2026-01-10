c39be45d9f6624e93c8dbc5b0d51a43e4a983c39
LbsLightX <shivamkumarking66@gmail.com>
2026-01-10 18:12:00 +0530

    Initial commit: 1llicit-colors repository

---
+++ README.md
@@ -0,0 +1,1 @@
+! 1llicit ! 1llicit ! 1llicit !
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+tmp_IFS=$IFS
+
+_require () {
+    for pkg in "$@"
+    do
+        command -v $pkg >/dev/null 2>&1 || { echo >&2 "I require '$pkg' but it's not installed. Aborting."; exit 1; }
+    done
+}
+
+_require jq curl fzf
+
+# Get themes from your new 1llicit-colors repository
+status_code=$(curl -s -o /dev/null -I -w "%{http_code}" "https://github.com/LbsLightX/1llicit-colors")
+if [ "$status_code" -eq "200" ]; then
+    echo "Fetching themes list from 1llicit-colors, please wait."
+    theme=$(curl -fSsL https://api.github.com/repos/LbsLightX/1llicit-colors/git/trees/main | jq -r '.tree[] | select (.path | contains(".properties")) | .path' | fzf)
+    if [ $? -eq 0 ]; then
+        echo "Applying color scheme: $theme"
+        mkdir -p ~/.termux
+        if curl -fsSL "https://raw.githubusercontent.com/LbsLightX/1llicit-colors/main/$theme" -o ~/.termux/colors.properties; then
+            termux-reload-settings
+            if [ $? -ne 0 ]; then
+                echo "Failed to apply color scheme."
+            fi
+        else
+            echo "Failed to download color scheme."
+        fi
+    else
+        echo "Cancelled color scheme selection."
+    fi
+else
+    echo "Make sure you're connected to the internet and your repo is public!"
+    exit 1
+fi
