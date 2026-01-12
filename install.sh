ad48e7169a0ad416d1e8ad1acdb09b9a8ff5c228
LbsLightX <shivamkumarking66@gmail.com>
2026-01-12 13:56:29 +0530

    UI: Remove clear command and add FZF header to installer

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ if [ "$status_code" -eq "200" ]; then
     echo "Fetching themes list from 1llicit-colors, please wait."
     
     # Updated Logic: Recursive search into 'themes/' folder
-    theme=$(curl -fSsL https://api.github.com/repos/LbsLightX/1llicit-colors/git/trees/main?recursive=1 | jq -r '.tree[] | select(.path | match("^themes/.*\\.properties$")) | .path' | fzf)
+    theme=$(curl -fSsL https://api.github.com/repos/LbsLightX/1llicit-colors/git/trees/main?recursive=1 | jq -r '.tree[] | select(.path | match("^themes/.*\\.properties$")) | .path' | fzf --prompt="Gogh Sync > " --height=15 --layout=reverse --header="[ Ctrl-c to Cancel ] | [ Enter to Apply ]")
     
-    if [ $? -eq 0 ]; then
+    if [ $? -eq 0 ] && [ -n "$theme" ]; then
         echo "Applying color scheme: $(basename $theme .properties)"
         mkdir -p ~/.termux
         # Download using the full path provided by jq (which includes "themes/...")
@@ -31,7 +31,7 @@ if [ "$status_code" -eq "200" ]; then
             echo "Failed to download color scheme."
         fi
     else
-        echo "Cancelled color scheme selection."
+        echo "⚠️ Cancelled."
     fi
 else
     echo "Make sure you're connected to the internet and your repo is public!"
